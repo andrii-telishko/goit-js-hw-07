9ce294ce87ce5863b530e03271c8cb50a378b4ed
Andrii Telishko <telishkoav@gmail.com>
2021-02-26 19:11:27 +0200

    Доробив task06

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,18 @@
 const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', onInputBlur);
+inputRef.addEventListener('focus', onInputFocus);
 
-function onInputBlur() { 
-    inputRef.value.length === Number(inputRef.getAttribute('data-length'))
-        ? inputRef.classList.add('valid')
-        : inputRef.classList.add('invalid');
+
+function onInputBlur() {
+console.log(inputRef.value.length);
+inputRef.value.length === Number(inputRef.getAttribute('data-length'))
+? inputRef.classList.add('valid')
+: inputRef.classList.add('invalid');
+}
+
+function onInputFocus() {
+inputRef.classList.remove('valid')
+inputRef.classList.remove('invalid')
 }
 
